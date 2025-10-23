5bb0b8484d26cfc5af8d27d2a872f8b89bfc7590
ichrakjaifra <ichrak.jaifra@gmail>
2025-10-23 12:26:07 +0100

    DOS-19 <Requêtes métier avancées>

--- src/main/java/com/delivery/repository/VehicleRepository.java
+++ src/main/java/com/delivery/repository/VehicleRepository.java
@@ -9,5 +9,14 @@ import java.util.List;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
+    List<Vehicle> findByType(Vehicle.VehicleType type);
 
+    @Query("SELECT v FROM Vehicle v WHERE v.id NOT IN (SELECT t.vehicle.id FROM Tour t WHERE t.date = CURRENT_DATE)")
+    List<Vehicle> findAvailableVehicles();
+
+    @Query("SELECT v FROM Vehicle v WHERE v.maxWeight >= :requiredWeight AND v.maxVolume >= :requiredVolume")
+    List<Vehicle> findSuitableVehicles(@Param("requiredWeight") Double requiredWeight,
+                                       @Param("requiredVolume") Double requiredVolume);
+
+    Vehicle findByLicensePlate(String licensePlate);
 }
